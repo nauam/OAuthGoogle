3c3d989c69a72fb32f54facd6fa7022645246c41
nauam <nauamvictor@outlook.com>
2022-03-16 15:07:25 -0300

    Return de dados do OAuth

--- oauth-google/src/App.js
+++ oauth-google/src/App.js
@@ -2,11 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 import GoogleLogin from 'react-google-login';
+import { Profiler, useState } from 'react';
 
 function App() {
 
+    const [email, setEmail] = useState()
+    const [familyName, setFamilyName] = useState()
+    const [givenName, setGivenName] = useState()
+    const [googleId, setGoogleId] = useState()
+    const [profilePic, setProfilePic] = useState()
+    const [name, setName] = useState()
+    const [tokenId, setTokenId] = useState()
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
     const responseGoogle = (response) => {
         console.log(response);
+        const {
+            profileObj: { email, familyName, givenName, googleId, imageUrl, name },
+        } = response
+
+        setEmail(email);
+        setFamilyName(familyName);
+        setGivenName(givenName);
+        setGoogleId(googleId);
+        setProfilePic(imageUrl);
+        setName(name);
+        setTokenId(tokenId);
+        setIsLoggedIn(true);
     };
 
     return (
@@ -20,6 +42,13 @@ function App() {
                     onFailure={responseGoogle}
                     cookiePolicy={'single_host_origin'}
                 />
+                {isLoggedIn ? <div>
+                    <h1>Informações do Usuário</h1>
+                    <img src={profilePic} alt="Profile" />
+                    <p>Nome: {name}</p>
+                    <p>Email: {email}</p>
+                    <p>GoogleId: {googleId}</p>
+                </div> : ''}
             </header>
         </div>
     );
